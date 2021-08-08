5015221cfd54d0474a6adc0f1cb97ee094b401a6
Fasih Mehmood <fasih.mehmood1@gmail.com>
2021-08-08 11:34:14 +0500

    fixed hamburger and mobile nav

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ window.onload = (event) => {
   console.log('page is fully loaded');
   const toggler = document.querySelector('.nav-toggler');
   const nav = document.querySelector('nav ul');
+  const togglerImg = document.querySelector('.nav-toggler img');
 
   toggler.addEventListener('click', () => {
     if (nav.classList.contains("opacity-1")) {
+      togglerImg.src = "./images/icon-hamburger.svg";
       nav.classList.toggle("opacity-1");
       setTimeout(() => {
         nav.classList.toggle("d-none");
       }, 700)
     } else {
+      togglerImg.src = "./images/icon-close.svg";
       nav.classList.toggle("d-none");
       setTimeout(() => {
         nav.classList.toggle("opacity-1");
